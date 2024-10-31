2c8d44c4c70a30c8ded0d8fe96b31841a4b0b4a9
Youssef Gaafar <yousfjaafer@gmail.com>
2024-10-31 15:59:13 +0300

    ➕ADD: Media Queries to improve responsiveness, and Container border

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-container.style.cssText = "display: flex; flex-wrap: wrap; margin-top: 90px; width: 100%; justify-content: center; margin-bottom: 90px;";
+container.style.cssText = "display: flex; flex-wrap: wrap; margin-top: 90px; width: 100%; justify-content: center; margin-bottom: 90px; border: 1px solid black;";
 const randomColor = () => Math.floor(Math.random() * 256);
 
 
@@ -10,6 +10,16 @@ const resetGrid = function () {
 }
 
 const hoverEffect = function (gridBoxes) {
+    if (window.matchMedia("(max-width: 768px)").matches) { // If screen width is less than 768px - Media Query for mobile
+        gridBoxes.forEach(gridBox => {
+            gridBox.addEventListener("click", () => {
+                const currentOpacity = parseFloat(gridBox.style.opacity);
+                gridBox.style.opacity = currentOpacity + 0.1;
+            });
+        });
+        return;
+    }
+
     gridBoxes.forEach(gridBox => {
         gridBox.addEventListener("mouseover", () => {
             const currentOpacity = parseFloat(gridBox.style.opacity);
@@ -33,7 +43,6 @@ function createCustomGrid() {
         for (let j = 0; j < gridSize; j++) {
             const gridBox = document.createElement("div");
             gridBox.classList.add(`grid-${i + 1}-${j + 1}`);
-            gridBox.textContent = `${i + 1}, ${j + 1}`;
             gridBox.style.cssText = `border: 1px solid black; width: ${newGridWidth}px; opacity: 0; height: ${newGridHeight}px;`;
             gridBox.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
             console.log("gridBox Width: ", gridBox.getBoundingClientRect().width);
@@ -42,16 +51,30 @@ function createCustomGrid() {
     }
     hoverEffect(container.querySelectorAll(".container div"));
 }
+const defaultMobileGridDisplay = function () {
+    const defaultGridWidth = Math.floor(container.getBoundingClientRect().width / 4.5);
+    const defaultGridHeight = defaultGridWidth;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            const gridBox = document.createElement("div");
+            gridBox.classList.add(`grid-${i + 1}-${j + 1}`);
+            gridBox.style.cssText = `border: 1px solid black; width: ${defaultGridWidth}px; opacity: 0; height: ${defaultGridHeight}px;`;
+            gridBox.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+            console.log("gridBox Width: ", gridBox.getBoundingClientRect().width);
+            container.appendChild(gridBox);
+        }
+    }
+    hoverEffect(container.querySelectorAll(".container div"));
+}
 
-const defaultGridDisplay = function () {
+const defaultWebGridDisplay = function () {
     const defaultGridWidth = Math.floor(container.getBoundingClientRect().width / 16.5);
     const defaultGridHeight = defaultGridWidth;
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             const gridBox = document.createElement("div");
             gridBox.classList.add(`grid-${i + 1}-${j + 1}`);
-            gridBox.textContent = `${i + 1}, ${j + 1}`;
-            gridBox.style.cssText = `border: 1px solid black; width: ${defaultGridWidth}px; opacity: 0; height: ${defaultGridHeight}px;`;
+            gridBox.style.cssText = `border: 1px solid black; width: ${defaultGridWidth}px; opacity: 0; height: ${defaultGridHeight}px; word-wrap: break-word;`;
             gridBox.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
             console.log("gridBox Width: ", gridBox.getBoundingClientRect().width);
             container.appendChild(gridBox);
@@ -62,8 +85,13 @@ const defaultGridDisplay = function () {
 
 console.log(container.getBoundingClientRect());
 
-//Default Grid Display
-defaultGridDisplay();
+//Default Grid Displays based on screen width
+if (window.matchMedia("(max-width: 768px)").matches) {
+    defaultMobileGridDisplay();
+}
+else {
+    defaultWebGridDisplay();
+}
 
 // Sketch Button
 const sketchButton = document.querySelector("button");
